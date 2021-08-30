3020c4d4566bb1da66c16d702525e8e014cb9147
Romain <rmurat.murat@gmail.com>
2021-08-30 18:57:36 +0200

    proxy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 const GotoClient = require('./structures/GotoClient');
-const fs = require('fs');
 require('dotenv').config()
 
-
-fs.writeFile('monitor', '',(err) => {
-    if (err) throw err;
-});
 
 let client = new GotoClient({
     prefix: '?',
--- src/util/VintedMonitor.js
+++ src/util/VintedMonitor.js
@@ -1,8 +1,8 @@
 const puppeteer = require("puppeteer");
-const { MessageEmbed, WebhookClient } = require('discord.js');
-const useProxy = require('puppeteer-page-proxy');
+const { MessageEmbed, WebhookClient, CommandInteractionOptionResolver } = require('discord.js');
 const fs = require('fs');
-const proxy_check = require('proxy-check');
+// const useProxy = require('puppeteer-page-proxy');
+// const proxy_check = require('proxy-check');
 
 
 
@@ -12,25 +12,26 @@ class VintedMonitor{
         this.url = url+"&order=newest_first";
         this.last_product_id = null;
         this.webhookClient = new WebhookClient({ url: discordWebhook});
-        this.proxyList = []
 
 
-        try {
-            const data = fs.readFileSync('proxy.json', 'utf8')
-            let proxyList = JSON.parse(data);
-            proxyList.forEach(proxyJson =>{
-                const proxy = {
-                    host: proxyJson.ip,
-                    port: proxyJson.port
-                  };
-                proxy_check(proxy).then(r => {
-                    this.proxyList.push(proxy)
-                }).catch(e => {
-                });
-            })
-        } catch (err) {
-            console.error(err)
-        }
+        // Proxy setup
+        // this.proxyList = []
+        // try {
+        //     const data = fs.readFileSync('proxy.json', 'utf8')
+        //     let proxyList = JSON.parse(data);
+        //     proxyList.forEach(proxyJson =>{
+        //         const proxy = {
+        //             host: proxyJson.ip,
+        //             port: proxyJson.port
+        //           };
+        //         proxy_check(proxy).then(r => {
+        //             this.proxyList.push(proxy)
+        //         }).catch(e => {
+        //         });
+        //     })
+        // } catch (err) {
+        //     console.error(err)
+        // }
     }
 
     async start(time = 30){
@@ -39,20 +40,32 @@ class VintedMonitor{
         this.browser = await puppeteer.launch();
         const page = await this.browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-        await page.setRequestInterception(true);
-        let countReq = 0;
-        page.on('request', async request => {
-            countReq++;
-            await useProxy(request, this.proxyList[countReq%this.proxyList.length]);
-        });
+
+        
         await page.goto(this.url);
         this.started = true;
+
+
+        //Proxy
+        // await page.setRequestInterception(true);
+        // let countReq = 0;
+
+        // page.on('request', async request => {
+        //     countReq++;
+        //     let proxy = this.proxyList[countReq%this.proxyList.length];
+        //     await useProxy(request, `${proxy.host}:${proxy.port}`);
+        // });
+
+
+        
+
+        
 
         //Boucle pour refresh vinted
         while(true === this.started){
             //Reload vinted
             await page.reload(this.url);
-            if(this.started === false) return
+            console.log(await page.content())
             //Reponse vinted en Json dans le tag script qui contient la propriété 'data-js-react-on-rails-store="MainStore"'
             let jsonText = await page.evaluate(() => document.querySelector('[data-js-react-on-rails-store="MainStore"]').innerHTML);
             let JSONResponse = JSON.parse(jsonText);
@@ -84,7 +97,7 @@ class VintedMonitor{
                     let status = element.status;
                     let price = element.price;
                     let size = element.size ? element.size : "Aucune";
-                    let img = element.photos[0].url;
+                    let img = element.photos[0].url ? element.photos[0].url : 'https://upload.wikimedia.org/wikipedia/commons/2/29/Vinted_logo.png';
                     let url = element.url;
                     let buyUrl = "https://www.vinted.fr/checkout?transaction_id="+id;
                     let offerUrl = "https://www.vinted.fr/items/"+id+"/want_it/new";
@@ -105,15 +118,18 @@ class VintedMonitor{
                             { name: 'Prix', value: price, inline: true },
                             { name: 'Condition', value: status, inline: true }
                         )
-                    await this.webhookClient.send({
-                        username: 'Vinted',
-                        avatarURL: 'https://www.vinted.fr/assets/favicon/default/favicon-32x32-3b949fa03c84fd2869f0fd9344005d5f3ff80c48a295d4f72f44ce3279931c89.png',
-                        embeds: [embed],
-                    });
+                    if(this.started === true) {
+                        await this.webhookClient.send({
+                            username: 'Vinted',
+                            avatarURL: 'https://www.vinted.fr/assets/favicon/default/favicon-32x32-3b949fa03c84fd2869f0fd9344005d5f3ff80c48a295d4f72f44ce3279931c89.png',
+                            embeds: [embed],
+                        });
+                    }
                 });
             }
             await this.sleep(time);
         };
+        console.log("test")
     }
 
     async stop(){
